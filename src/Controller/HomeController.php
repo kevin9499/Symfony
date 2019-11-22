8c1f561583ad04d7b3ae7449aaca3e7dff8bc0d5
kevin9499 <kevin9402@gmail.com>
2019-11-22 11:49:18 +0100

    first

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/vente", name="vente")
+     */
+    public function home()
+    {
+        return $this->render('home/vente.html.twig');
+    }
+
+}
